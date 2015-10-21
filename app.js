5ac2f29ee13d88fc6fabde692dbe5b3617d8e331
Antiokus314 <navneetgarg123@gmail.com>
2015-10-21 14:19:43 -0400

    Add resource routing and configure controllers

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
+var env = process.env.NODE_ENV || 'development';
+process.env.DEBUG = env === 'development' ? '*:server' : '';
+
 var express = require('express');
-
 var app = express();
+app.set('env', env);
+
 var path = require('path');
-
-
+var routing = require('resource-routing');
+var controllers_dir = path.resolve('./app/controllers');
 
 var middleware = require('./middleware')();
 
 middleware.load(app, function(a) {
-  a.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express Seed' });
-  });
+  routing.root(app, controllers_dir, 'home', 'index');
+
+  if (app.get('env') === 'development') {
+    routing.expose_routing_table(a);
+  }
 });
 
 module.exports = app;
