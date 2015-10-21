cfb90368677796b01447a2c63c8a2d288b824d8a
Antiokus314 <navneetgarg123@gmail.com>
2015-10-21 13:55:02 -0400

    Update middleware loading

--- app.js
+++ app.js
@@ -3,17 +3,14 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-var Middleware = require('./middleware');
-var m = new Middleware(app, path);
 
-m.basic();
 
-m.assets();
-app.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express Seed' });
+var middleware = require('./middleware')();
+
+middleware.load(app, function(a) {
+  a.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express Seed' });
+  });
 });
-
-m.errors();
-
 
 module.exports = app;
--- middleware/index.js
+++ middleware/index.js
@@ -2,40 +2,32 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var Mincer = require('mincer');
+var path = require('path');
 
-function MiddlewareLoader(app, path) {
-  this.app = app;
-  this.path = path;
+function createAssetPipeline(app) {
+  var env = new Mincer.Environment(path.join(__dirname, '..'));
+  env.appendPath(path.join(__dirname, '..', 'app', 'assets', 'stylesheets'))
+  env.appendPath(path.join(__dirname, '..', 'app', 'assets', 'javascripts'))
+  env.appendPath(path.join(__dirname, '..', 'app', 'assets', 'images'))
+  app.use('/assets', Mincer.createServer(env));
 }
 
-MiddlewareLoader.prototype.basic = function basic() {
-  var self = this;
-  self.app.set('views', self.path.join(__dirname, '..', 'app', 'views'));
-  self.app.set('view engine', 'jade');
+function loadBasicSettings(app) {
+  app.set('views', path.join(__dirname, '..', 'app', 'views'));
+  app.set('view engine', 'jade');
 
   // uncomment after placing your favicon in /public
   //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-  self.app.use(logger('dev'));
-  self.app.use(bodyParser.json());
-  self.app.use(bodyParser.urlencoded({ extended: false }));
-  self.app.use(cookieParser());
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
   //app.use(express.static(path.join(__dirname, 'public')));
-};
+}
 
-MiddlewareLoader.prototype.assets = function assets() {
-  var self = this;
-  var env = new Mincer.Environment(self.path.join(__dirname, '..'));
-  env.appendPath(self.path.join(__dirname, '..', 'app', 'assets', 'stylesheets'))
-  env.appendPath(self.path.join(__dirname, '..', 'app', 'assets', 'javascripts'))
-  env.appendPath(self.path.join(__dirname, '..', 'app', 'assets', 'images'))
-
-  self.app.use('/assets', Mincer.createServer(env));
-};
-
-MiddlewareLoader.prototype.errors = function errors() {
-  var self = this;
+function loadErrors(app) {
   // catch 404 and forward to error handler
-  self.app.use(function(req, res, next) {
+  app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -45,8 +37,8 @@ MiddlewareLoader.prototype.errors = function errors() {
 
   // development error handler
   // will print stacktrace
-  if (self.app.get('env') === 'development') {
-    self.app.use(function(err, req, res, next) {
+  if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
       res.status(err.status || 500);
       res.render('error', {
         message: err.message,
@@ -57,13 +49,26 @@ MiddlewareLoader.prototype.errors = function errors() {
 
   // production error handler
   // no stacktraces leaked to user
-  self.app.use(function(err, req, res, next) {
+  app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
       error: {}
     });
   });
+}
+
+var middleware = {
+  load: function load(app, cb) {
+    loadBasicSettings(app);
+    createAssetPipeline(app);
+    if (cb) {
+      cb(app);
+    }
+    loadErrors(app);
+  }
 };
 
-module.exports = MiddlewareLoader;
+module.exports = function() {
+  return (Object.create(middleware));
+};
