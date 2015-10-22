780ed273e762accc3ca3175051e3bf25653c1f93
Antiokus314 <navneetgarg123@gmail.com>
2015-10-21 22:22:25 -0400

    Update route interface

--- app/middleware/routes.js
+++ app/middleware/routes.js
@@ -1,10 +1,17 @@
-var routing = require('resource-routing');
+var routing = require('express-resource-routing');
 var routes = require('../../config/routes');
+var path = require('path');
+var controllersDir = path.resolve(path.join(__dirname, '..', 'controllers'));
 
 var middleware = {
   load: function(app) {
     routes.forEach(function(route) {
-      routing[route[0]](app, route[1], route[2], route[3]);
+      if (route.length === 3) {
+        routing[route[0]](app, controllersDir, route[1], route[2]);
+      }
+      if (route.length === 4) {
+        routing[route[0]](app, route[1], route[2], route[3]);
+      }
     });
 
     if (app.get('env') === 'development') {
--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,4 @@
-var path = require('path');
-var defaultControllersDir = path.resolve(path.join(__dirname, '..', 'app','controllers'));
 
 module.exports = [
-  ["root", defaultControllersDir, "home", "index"]
+  ["root", "home", "index"]
 ];
