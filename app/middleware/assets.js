f42512f497a5d61efcb976f959909cd60315d395
Antiokus314 <navneetgarg123@gmail.com>
2015-11-07 18:45:49 -0500

    Add AssetMiddleware documentation

--- app/middleware/assets.js
+++ app/middleware/assets.js
@@ -1,20 +1,45 @@
+/**
+ * AssetMiddleware Singleton
+ * uses AssetConfig, app
+ */
 var Mincer = require('mincer');
 var path = require('path');
 var env = new Mincer.Environment(path.join(__dirname, '..', '..'));
 
+// inject dependencies
 exports.inject = ['AssetConfig', 'app'];
 exports.load = function(AssetConfig, app) {
+  // load macro-processor config (allows the usage of "$$ asset $$" in specified extensions
   Mincer.MacroProcessor.configure(AssetConfig.assetMacroProcessor);
 
+  /**
+   * @class AssetMiddleware
+   * @classdesc middleware singleton object
+   */
   var AssetMiddleware = {
+    /**
+     * @static loadAssetPaths
+     * load the asset paths defined in the AssetConfig
+     */
     loadAssetPaths: function() {
       AssetConfig.assetPaths.forEach(function(p) {
         env.appendPath(p);
       });
     },
+    /**
+     * @static createAssetPipeline
+     * to be called after loading asset paths. Creates a local server instance to serve up assets
+     * gets loaded into the app instance as middleware
+     */
     createAssetPipeline: function() {
       app.use(AssetConfig.mountPoint, Mincer.createServer(env));
     },
+    /**
+     * @static findAsset
+     * @param {string} file
+     * @param {object} [opts]
+     * @return {string} path to asset file
+     */
     findAsset: function(file, opts) {
       var asset = env.findAsset(file, opts);
       if (!asset) {
@@ -23,20 +48,27 @@ exports.load = function(AssetConfig, app) {
         return path.join(AssetConfig.mountPoint, asset.digestPath);
       }
     },
-    /*
-     * js helper (uses findAsset)
+    /**
+     * @static jsAsset
+     * @param {string} file
+     * @param {object} opts
+     * @return {string} js script tag with loaded asset path
      */
     jsAsset: function(file, opts) {
       return '<script type="application/javascript" src="' + AssetMiddleware.findAsset(file, opts) + '"></script>';
     },
-    /*
-     * css helper (uses findAsset)
+    /**
+     * @static cssAsset
+     * @param {string} file
+     * @param {object} opts
+     * @return {string} css link tag with loaded asset path
      */
     cssAsset: function cssAsset(file, opts) {
       return '<link type="text/css" rel="stylesheet" href="' + AssetMiddleware.findAsset(file, opts) + '"/>';
     }
   }
 
+  // run AssetMiddleware
   AssetMiddleware.loadAssetPaths();
   AssetMiddleware.createAssetPipeline();
   env.registerHelper('asset_path', AssetMiddleware.findAsset);
