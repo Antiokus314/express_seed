8a92c57443eb3f15bc5e4cacaf731d7901376a7e
Antiokus314 <navneetgarg123@gmail.com>
2015-11-07 20:11:04 -0500

    Add generator for middleware

---
+++ templates/middleware.js
@@ -0,0 +1,15 @@
+/**
+ * MIDDLEWARE Singleton
+ * uses app
+ */
+
+/**
+ * @class MIDDLEWARE
+ */
+var MIDDLEWARE = {
+  inject: 'app',
+  load: function(app) {
+  }
+};
+
+module.exports = MIDDLEWARE;
